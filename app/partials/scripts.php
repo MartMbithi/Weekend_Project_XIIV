685c07b1baca39806d6b0d82076ac943d7336984
MartMbithi <martdevelopers254@gmail.com>
2023-06-22 17:46:30 +0300

    Implement Login Pages UI

---
+++ app/partials/scripts.php
@@ -0,0 +1,8 @@
+<script src="../public/vendor/global/global.min.js"></script>
+<script src="../public/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+<script src="../public/js/custom.min.js"></script>
+<script src="../public/js/deznav-init.js"></script>
+
+<!-- Datatable -->
+<script src="../public/vendor/datatables/js/jquery.dataTables.min.js"></script>
+<script src="../public/js/plugins-init/datatables.init.js"></script>
