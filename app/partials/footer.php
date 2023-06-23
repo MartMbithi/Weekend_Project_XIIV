7eb9714ae84fae21cf48513813d0c472ed6bd43f
MartMbithi <martdevelopers254@gmail.com>
2023-06-23 05:49:43 +0300

    Implement Project Scope

---
+++ app/partials/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright © <?php echo date('Y'); ?>. Veterinary Clinic Management System</p>
+    </div>
+</div>
---
+++ app/partials/sidebar.php
@@ -0,0 +1,46 @@
+<div class="deznav">
+    <div class="deznav-scroll">
+        <ul class="metismenu" id="menu">
+            <li><a href="dashboard" class="ai-icon" aria-expanded="false">
+                    <i class="fa fa-home fa-2x"></i>
+                    <span class="nav-text">Dashboard</span>
+                </a>
+            </li>
+            <li><a href="doctors" class="ai-icon" aria-expanded="false">
+                    <i class="fa fa-user-md"></i>
+                    <span class="nav-text">Vet Doctors</span>
+                </a>
+            </li>
+            <li><a href="clients" class="ai-icon" aria-expanded="false">
+                    <i class="fa fa-users"></i>
+                    <span class="nav-text">Clients</span>
+                </a>
+            </li>
+            <li><a href="pets" class="ai-icon" aria-expanded="false">
+                    <i class="fa fa-paw"></i>
+                    <span class="nav-text">Pets</span>
+                </a>
+            </li>
+            <li><a href="treatments" class="ai-icon" aria-expanded="false">
+                    <i class="fa fa-calendar"></i>
+                    <span class="nav-text">Treatments</span>
+                </a>
+            </li>
+            <li><a href="payments" class="ai-icon" aria-expanded="false">
+                    <i class="fa fa-money"></i>
+                    <span class="nav-text">Payments</span>
+                </a>
+            </li>
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="fa fa-file"></i>
+                    <span class="nav-text">Reports</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="reports_clients">Clients</a></li>
+                    <li><a href="reports_treatments">Treatments</a></li>
+                    <li><a href="reports_payments">Payments</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
